49acc61ccccdc70c0acddd52b4f206510ac49b33
Kursat <75574588+DKursat@users.noreply.github.com>
2022-05-07 00:03:58 +0200

    Summary(Pruefungsvorbereitung!)

---
+++ Schule/Pruefungsvorbereitung/Pruefungsvorbereitung/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pruefungsvorbereitung
+{
+    public partial class Form1 : Form
+    {
+        Testbereich testbereich = new Testbereich();
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+            richTextBox3.Text = "";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            testbereich.Datenbankverbindung();
+
+            string success = "Die Verbindung wurde erfolgreich hergestellt!!";
+            MessageBox.Show(success);
+
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            testbereich.Datenbankverbindungschließen();
+        }
+    }
+}
---
+++ Schule/Pruefungsvorbereitung/Pruefungsvorbereitung/Testbereich.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Pruefungsvorbereitung
+{
+    internal class Testbereich
+    {
+        private readonly string verbindungsstring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:/Github/Visual_studio_community/Schule/Pruefungsvorbereitung/Pruefungsvorbereitung/BspDB.mdb";
+        protected bool offen = false;
+        OleDbConnection conn = null;
+        public void Datenbankverbindung()
+        {
+         
+            try
+            {
+                conn= new OleDbConnection(verbindungsstring);
+                conn.Open();
+               offen = true;
+
+            }
+            catch (Exception excep)
+            {
+             
+                MessageBox.Show(Convert.ToString(excep));
+            }
+        }
+
+        public void Datenbankverbindungschließen()
+        {
+            if (offen == true)
+            {
+                
+                conn = new OleDbConnection(verbindungsstring); 
+                conn.Close();
+                string geschlossen = "Die Verbindung wurde erfolgreich geschlossen!!";
+                MessageBox.Show(geschlossen);
+            }
+
+            else
+            {
+                MessageBox.Show("Bool hat nicht geklappt kontaktiere denn Hersteller der hat es versaut!!!");
+            }
+            
+            
+        }
+
+
+
+
+
+
+
+    }
+}
